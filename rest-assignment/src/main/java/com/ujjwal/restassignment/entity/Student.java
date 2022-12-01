c0f48651668a2e5227c996a7e52f6f10286d895e
deStarxis <humagainuzjwol@gmail.com>
2022-11-30 21:59:30 -0600

    added student entity, operations and APIs

--- rest-assignment/src/main/java/com/ujjwal/restassignment/entity/Student.java
+++ rest-assignment/src/main/java/com/ujjwal/restassignment/entity/Student.java
@@ -1,12 +1,16 @@
 package com.ujjwal.restassignment.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
     private int id;
     private String firstName;
--- rest-assignment/src/main/java/com/ujjwal/restassignment/service/StudentService.java
+++ rest-assignment/src/main/java/com/ujjwal/restassignment/service/StudentService.java
@@ -1,4 +1,24 @@
 package com.ujjwal.restassignment.service;
 
+import com.ujjwal.restassignment.dto.CourseDto;
+import com.ujjwal.restassignment.dto.StudentDto;
+import com.ujjwal.restassignment.entity.Student;
+
+import java.util.List;
+
 public interface StudentService {
+    //    adding a new student
+    void save(StudentDto studentDto);
+    //    updating a student
+    StudentDto update(int id, StudentDto studentDto);
+    //    deleting a student
+    void delete(int id);
+    //    getting a single course
+    StudentDto getById(int id);
+    //    getting all the courses
+    List<StudentDto> getAll();
+    //    get all students by major
+    List<StudentDto> getStudentsByMajor(String major);
+    //    get all courses of the student
+    List<CourseDto> getCoursesByStudentId(int studentId);
 }
