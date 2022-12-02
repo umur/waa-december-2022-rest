7171fa32bfc4b28841697f3982f93b40054e677e
anhaabc <anhaa0821@gmail.com>
2022-12-02 01:40:35 -0600

    rest services completed

---
+++ src/test/java/waa/lab2/WaaDecember2022RestApplicationTests.java
@@ -0,0 +1,13 @@
+package waa.lab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaaDecember2022RestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
