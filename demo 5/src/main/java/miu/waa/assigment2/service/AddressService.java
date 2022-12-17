ec8f61363dab7fdd1418115ab6948477a0acf92e
Niqedimos <niqedimosghe584@gmail.com>
2022-12-16 20:29:26 -0600

    My Rest Assignment

--- demo 5/src/main/java/miu/waa/assigment2/service/AddressService.java
+++
@@ -1,4 +0,0 @@
-package miu.waa.assigment2.service;
-
-public interface AddressService {
-}
--- demo 5/src/main/java/miu/waa/assigment2/service/impl/AddressServiceImpl.java
+++
@@ -1,6 +0,0 @@
-package miu.waa.assigment2.service.impl;
-
-import miu.ea.assigment3.service.AddressService;
-
-public class AddressServiceImpl implements AddressService {
-}
